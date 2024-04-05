05324544df9b4244aaa2f9e5eef02b477c7f6e7d
unknown <albpe966@student.liu.se>
2024-04-05 16:20:08 +0200

    lite progress, kör på contours istället

--- image_location.py
+++ image_location.py
@@ -1,14 +1,58 @@
 import cv2
 import numpy as np
+import pyautogui
+board_color = [189, 189, 189]
 
-def find_template_in_image(template, image, threshold = 0.5, method_input = cv2.TM_CCOEFF_NORMED):
-    
-    method = method_input
-    res = cv2.matchTemplate(image, template, method)
+def find_game_board_coords():
+    #Used to find the game boarders
 
-    max_val, max_loc = cv2.minMaxLoc(res)[1], cv2.minMaxLoc(res)[3]
-    if max_val >= threshold:
-        print(f'Template found with value: {max_val}')
-        cv2.imshow('Template', template)
-    else:
-        print('Template not found in image.')
+    screenshot = pyautogui.screenshot()
+    screenshot = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2BGR) #cv2 uses BGR instead of RGB which we screenshot in
+    x, y, w, h = find_biggest_contour(screenshot, board_color)
+    return x, y, w, h
+
+def find_all_positions(image):
+    image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR) #cv2 uses BGR instead of RGB which we screenshot in
+    contours = find_all_contour(image)
+    positions = []
+    for contour in contours:
+        x, y, w, h = cv2.boundingRect(contour)
+        if w == h:
+            positions.append((x, y, w, h))
+    for position in positions:
+        x, y, w, h = position
+        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 3)
+    cv2.imshow('All Positions', image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    return positions
+
+def find_all_contour(image, color=board_color):
+    lower_bound = np.array([color[2]-1, color[1]-1, color[0]-1]) #Converts our RGB to BGR
+    upper_bound = np.array([color[2]+1, color[1]+1, color[0]+1])
+
+    mask = cv2.inRange(image, lower_bound, upper_bound)
+    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    return contours
+
+
+def find_biggest_contour(image, color):
+    lower_bound = np.array([color[2]-1, color[1]-1, color[0]-1]) #Converts our RGB to BGR
+    upper_bound = np.array([color[2]+1, color[1]+1, color[0]+1])
+
+    mask = cv2.inRange(image, lower_bound, upper_bound)
+    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    biggest_contour = None
+    biggest_area = 0
+    for contour in contours:
+        area = cv2.contourArea(contour)
+        if area > biggest_area:
+            biggest_area = area
+            biggest_contour = contour
+    x, y, w, h = cv2.boundingRect(biggest_contour)
+    return x, y, w, h
+     
+    #image_with_contours = cv2.drawContours(image.copy(), [biggest_contour], -1, (0, 255, 0), 3)
+    #cv2.imshow('Biggest Contour', image_with_contours)
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
--- main.py
+++ main.py
@@ -1,23 +1,30 @@
 from image_location import *
+import time
+import keyboard
+import pyautogui
 
+#Image template
 test_template = "img/board100.png"
 test_image = "img/fake_screen.png"
 
-template = cv2.imread(test_template, cv2.IMREAD_GRAYSCALE)
-image = cv2.imread(test_image, cv2.IMREAD_GRAYSCALE)
-
-def resize_image(image, scale):
-    width = int(image.shape[1] * scale)
-    height = int(image.shape[0] * scale)
-    dim = (width, height)
-    return cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
+#Image loaded with cv2
+template = cv2.imread(test_template)
+image = cv2.imread(test_image)
 
 if __name__ == '__main__':
     print('Starting...')
-    print("Original size:")
-    find_template_in_image(template, image)
+    time.sleep(3)
 
-    print("Resized 2x:")
-    resized_template = resize_image(image, 0.3)
-    find_template_in_image(resized_template, image)
+    while keyboard.is_pressed('q') == False:
+        game_board_region = find_game_board_coords() # (x, y, w, h)
+        screenshot = pyautogui.screenshot(region=game_board_region)
+        
+        image = cv2.cvtColor(np.array(screen), cv2.COLOR_RGB2BGR) #cv2 uses BGR instead of RGB which we screenshot in
+        x, y, w, h = find_biggest_contour(image, [123,123,123])
+        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 3)
+        cv2.imshow('All Positions', image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        print("Done, waiting 10 seconds...")
+        time.sleep(10)
     print("Done!")
